f679612a00c524e84a125ba97a505fda6826cc2a
Hayyanshaikh <hayyanshaikh55@gmail.com>
2026-01-15 01:38:07 +0500

    Install prisma, prisma client and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({ origin: 'http://localhost:3001' });
+
+  const config = new DocumentBuilder()
+    .setTitle('My School APIs')
+    .setDescription('My School API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
